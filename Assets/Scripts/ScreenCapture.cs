2e09f50cbae1348c9417df308fd6994aefc87cee
54763755@qq.com <54763755@qq.com>
2021-06-28 09:40:53 +0800

    1.上传基于unity自带的截屏方法的测试。

---
+++ Assets/Scripts/ScreenCapture.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenCapture : MonoBehaviour
+{
+    [SerializeField]
+    private Text status;
+    [SerializeField]
+    private Text buttonText;
+    [SerializeField]
+    private bool autoClearFolder = true;
+    private int captureTimes = 0;
+    private DateTime startTime = DateTime.Now;
+    private bool isRunning = false;
+    private string filePath
+    {
+        get
+        {
+            return string.Format("{0}/screen_capture", Application.persistentDataPath);
+        }
+    }
+    private string fileName
+    {
+        get
+        {
+            return string.Format("{0}/screen_capture_{1}.png", this.filePath, DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss-fff"));
+        }
+    }
+
+    private void Start()
+    {
+    }
+
+    public void Run()
+    {
+        this.isRunning = !this.isRunning;
+        if (this.isRunning)
+        {
+            this.buttonText.text = "Stop";
+            if (Directory.Exists(this.filePath))
+            {
+                foreach (string file in Directory.GetFiles(this.filePath))
+                {
+                    File.Delete(file);
+                }
+            }
+
+            this.StopAllCoroutines();
+            this.StartCoroutine(this.CaptureScreen());
+
+        }
+        else
+        {
+            this.buttonText.text = "Start";
+            this.StopAllCoroutines();
+        }
+    }
+
+    private IEnumerator CaptureScreen()
+    {
+        this.startTime = DateTime.Now;
+        this.captureTimes = 1;
+
+        while (this.isRunning)
+        {
+            UnityEngine.ScreenCapture.CaptureScreenshot(this.fileName);
+            this.status.text = string.Format("remain: {0:f1}s, capture times: {1}", (DateTime.Now - this.startTime).TotalSeconds, this.captureTimes);
+            this.captureTimes += 1;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/ScreenSaver.cs
@@ -0,0 +1,37 @@
+﻿using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenSaver : MonoBehaviour
+{
+    [SerializeField]
+    private Image imageUp;
+    [SerializeField]
+    private Image imageDown;
+    [SerializeField, Range(1f, 5f)]
+    private float interval = 1f;
+    [SerializeField]
+    private List<Sprite> imageList;
+    private int index;
+
+    private void Start()
+    {
+        this.imageUp.sprite = this.imageList[0];
+        this.imageDown.sprite = this.imageList[1];
+        this.index = 1;
+
+        this.InvokeRepeating("RefreshScreen", 0f, this.interval);
+    }
+
+    private void RefreshScreen()
+    {
+        int nextIndex = (this.index + 1) % this.imageList.Count;
+        this.imageUp.DOFade(0f, 0.5f).onComplete = () =>
+        {
+            this.imageUp.sprite = this.imageDown.sprite;
+            this.imageDown.sprite = this.imageList[nextIndex];
+            this.index = nextIndex;
+        };
+    }
+}
